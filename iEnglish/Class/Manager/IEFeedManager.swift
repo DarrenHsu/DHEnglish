74c204891d2b30281c54a2554867cfe5f3d1890a
Dareen <skcu1805@skl.com.tw>
2016-10-13 17:22:24 +0800

    update code

--- iEnglish/Class/Manager/IEFeedManager.swift
+++ iEnglish/Class/Manager/IEFeedManager.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 //let ResourceURL = "http://localhost:8888/wjson"
-let ResourceURL = "http://192.168.1.103:8888/wjson"
+//let ResourceURL = "http://192.168.1.103:8888/wjson"
+let ResourceURL = "http://172.20.10.4:8888/wjson"
 
 class IEFeedManager: NSObject {
     static let sharedInstance = IEFeedManager()
